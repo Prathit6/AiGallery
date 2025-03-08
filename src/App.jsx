b0031e9c422c23b5207abd37615be46aa9d7dcab
Prathit Dode <prathitdode@gmail.com>
2025-03-08 11:51:55 +0530

    initialize project with Vite, React, and TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="bg-amber-400 absolute top-1/2 left-1/2 ">Our Ai gallery Project</div>
+      
+    </>
+  )
+}
+
+export default App
